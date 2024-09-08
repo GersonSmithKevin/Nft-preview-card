1677af26b0013f37c432100bb3d18c8b8ca5302a
GersonSmithKevin <Gersoncamacho49@gmail.com>
2024-09-07 21:14:30 -0500

    chore: finalize configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import logo from './logo.svg';
-import './App.css';
+// import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  // return (
+  //   <article>
+  //     <header>
+  //       <img src="https://unavatar.io/kikobeats" />
+  //       <div>
+  //         <strong>Gerson Camacho</strong>
+  //         <a href='https://github.com/GersonSmithKevin'> Github:GersonSmithKevin</a>
+  //       </div>
+  //     </header>
+
+  //   </article>
+  // )
+  //aqui se escribe el progama principal apra se enviado al indes.js asi index solo tendra app()
+
 }
+
+
+
+
 
 export default App;
